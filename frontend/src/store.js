9b550807e322d91e0590bda28460e3618065d9fa
KKA BALAJI <balaji.kka@gmail.com>
2021-10-23 19:22:20 +0530

    Add redux and list products

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import {
+  productDetailsReducer,
+  ProductListReducer,
+} from "./reducers/productReducer";
+const reducer = combineReducers({
+  productList: ProductListReducer,
+  productDetails: productDetailsReducer,
+});
+
+const initialState = {};
+const middleWare = [thunk];
+export const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
